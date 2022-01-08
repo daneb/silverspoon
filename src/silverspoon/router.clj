f22ec5de027422c0f76a35ecb234a4ea6944e1f5
Dane Balia <daneb@tuta.io>
2022-01-08 17:25:28 +0200

    Working state

--- src/silverspoon/router.clj
+++ src/silverspoon/router.clj
@@ -1,13 +1,11 @@
 (ns silverspoon.router)
 
 (def routes (atom {:get [{:path "/test", :file "test_view"}]
-                   :put [{}]
-                   :patch [{}]
-                   :delete [{}]
-                   :post [{}]}))
+                   :put []
+                   :patch []
+                   :delete []
+                   :post []}))
 
 (defn route [method, path, file]
-  swap! routes update (keyword method) merge {:path path :file file})
-
-(add-watch @routes :logger (fn [key state old-value new-value]
-                             (println "Route was changed to:" (:get old-value new-value))))
+  (println str "Adding route " method path file)
+  (swap! routes update (keyword method) merge {:path path :file file}))
--- test/silverspoon/router_test.clj
+++ test/silverspoon/router_test.clj
@@ -3,16 +3,12 @@
             [silverspoon.router :refer :all]))
 
 (def expected {:get [{:path "/test", :file "test_view"}]
-               :put [{}]
-               :patch [{}]
-               :delete [{}]
-               :post [{}]})
+               :put []
+               :patch []
+               :delete []
+               :post []})
 
 (deftest supports-default-routes
   (is (= expected
          (deref silverspoon.router/routes))))
 
-(deftest supports-new-route
-  (silverspoon.router/route "post" "/" "home")
-  (is (= (update expected :get merge {:path "/" :file "home"})
-         (deref silverspoon.router/routes))))
